b3eb9f48eed37d695b092e448a68094b9544b3b1
Raghu <raghuisavailable@outlook.com>
2025-09-18 00:35:24 +0530

    assembly ref changes

--- ODP_Backend/ODP_Studio_Api/ODP_Studio_Api.Application/obj/Debug/net8.0/ODP_Studio_Api.Application.AssemblyInfo.cs
+++ ODP_Backend/ODP_Studio_Api/ODP_Studio_Api.Application/obj/Debug/net8.0/ODP_Studio_Api.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ODP_Studio_Api.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a58a868a0ef17ac96a5b4aa572d50c685ba127c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea5cf48974c68608733419f158af84cb86edba0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ODP_Studio_Api.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ODP_Studio_Api.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
