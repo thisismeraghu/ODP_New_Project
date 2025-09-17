f8d90ee4c5c7563b78715e38584a13f50c0eacbf
unknown <ddoyijodenayana@gmail.com>
2025-09-17 22:24:53 +0530

    fix added text

--- ODP_Frontend/odp-app/src/components/Footer.tsx
+++ ODP_Frontend/odp-app/src/components/Footer.tsx
@@ -17,7 +17,7 @@ const Footer: React.FC<FooterProps> = ({ isAuthenticated }) => (
     <Typography variant="body2" color="text.secondary">
       {isAuthenticated
         ? `© ${new Date().getFullYear()} ODP Dashboard`
-        : `© ${new Date().getFullYear()} TEXVEX Software Solutions`}
+        : `© ${new Date().getFullYear()} this is TEXVEX Software Solutions`}
     </Typography>
   </Box>
 );
